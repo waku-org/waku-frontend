249f678ab5677ae0aaabc2991d9f91acdb3adc33
Sasha <oleksandr@status.im>
2023-10-30 23:57:10 +0100

    copy rln-js

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+import protobuf from "protobufjs";
+
+export type ProtoChatMessageType = {
+  timestamp: number;
+  nick: string;
+  text: string;
+};
+
+export const ProtoChatMessage = new protobuf.Type("ChatMessage")
+  .add(new protobuf.Field("timestamp", 1, "uint64"))
+  .add(new protobuf.Field("nick", 2, "string"))
+  .add(new protobuf.Field("text", 3, "string"));
+
+export const CONTENT_TOPIC = "/toy-chat/2/luzhou/proto";
+
+export const SIGNATURE_MESSAGE =
+  "The signature of this message will be used to generate your RLN credentials. Anyone accessing it may send messages on your behalf, please only share with the RLN dApp";
