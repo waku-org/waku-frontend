5a651a4119e2d19cd1d6837a30a37f4c86c602e1
Sasha <oleksandr@status.im>
2023-12-06 00:18:29 +0530

    bug: allow to publish to set contentTopic

--- src/hooks/useWaku.ts
+++ src/hooks/useWaku.ts
@@ -64,7 +64,7 @@ export const useWaku = () => {
       await waku.relay.send({
         version: 0,
         timestamp,
-        contentTopic: CONTENT_TOPIC,
+        contentTopic,
         payload: btoa(JSON.stringify({
           nick,
           text
